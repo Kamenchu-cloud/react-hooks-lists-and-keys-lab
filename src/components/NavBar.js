1c2adb5d6fc89525a0b8f47fdaa4c1d8bf2aeca4
Kamenchu <iankamenchu@gmail.com>
2024-02-03 10:12:36 +0300

    correct first deliverable

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>
-    <a key={links[0]} href={`#${links[0]}`}>{links[0]}</a>
-    <a key={links[1]} href={`#${links[1]}`}>{links[1]}</a>
-    <a key={links[2]} href={`#${links[2]}`}>{links[2]}</a>
-  </nav>
+  return (
+    <nav>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
